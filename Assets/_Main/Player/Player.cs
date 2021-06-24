104ace3cca6d0d7dbd796980ef023fcc2e30ec84
Edwin Jones <edwin.jones@digitalpages.com.br>
2021-06-24 00:29:31 -0300

    Created player

---
+++ Assets/_Main/Player/Player.cs
@@ -0,0 +1,38 @@
+using EdwinGameDev.Movement;
+using EdwinGameDev.Projectile;
+using EdwinGameDev.Settings;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EdwinGameDev.Player
+{
+    public class Player : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private AMovementController movementController;
+
+        private void Awake()
+        {
+            gameObject.tag = Tags.PLAYER;
+        }
+
+        private void Update()
+        {
+            movementController?.Move(Time.deltaTime);
+        }
+
+        public void ReceiveDamage(int value)
+        {
+            Debug.Log("player Damage: "+value);
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag(Tags.BULLET))
+            {
+                col.GetComponent<IBullet>().ApplyDamage(this);
+            }
+        }
+    }
+}
