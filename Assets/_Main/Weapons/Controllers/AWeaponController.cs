a01df2b42f3949c8361cbd627b5476e0417c6aa8
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-06-27 20:07:22 -0300

    Refactor AWeapon also added IObserver to it

--- Assets/_Main/Weapons/Controllers/AWeaponController.cs
+++ Assets/_Main/Weapons/Controllers/AWeaponController.cs
@@ -1,38 +1,41 @@
-﻿using UnityEngine;
+﻿using EdwinGameDev.Projectile;
+using UnityEngine;
 
 namespace EdwinGameDev.Weapons
 {
-    public abstract class AWeaponController : MonoBehaviour
+    public abstract class AWeaponController : MonoBehaviour, IObserver
     {
         public GameObject bulletPrefab;
+        protected AProjectile projectile;
+
         public abstract IWeapon GetWeapon();
         [SerializeField] protected int poolSize = 30;
 
-        private Pooling bulletPool;
-        protected GameObject currentBullet;
+        private Pooling projectilePool;
 
         public abstract void Shoot();
+        public virtual void ReceiveNotification(ISubject subject) { }
 
         protected void ShootFromPosition(Transform shootingPoint)
         {
-            if (bulletPool == null)
+            if (projectilePool == null)
             {
                 CreateBulletPool(poolSize);
             }
-            else
+
+            // Get bullet from pool
+            projectile = projectilePool.GetFromPool() as AProjectile;
+
+            if (projectile != null)
             {
-                // Get bullet from pool
-                currentBullet = bulletPool.GetFromPool();
+                projectile.Attach(this);
 
-                if (currentBullet != null)
-                {
-                    // Assign shooting point values
-                    currentBullet.transform.position = shootingPoint.position;
-                    currentBullet.transform.rotation = shootingPoint.rotation;
+                // Assign shooting point values
+                projectile.transform.position = shootingPoint.position;
+                projectile.transform.rotation = shootingPoint.rotation;
 
-                    // Reset        
-                    currentBullet = null;
-                }
+                // Reset        
+                projectile = null;
             }
         }
 
@@ -42,8 +45,10 @@ namespace EdwinGameDev.Weapons
             GameObject poolGo = new GameObject("Pool_" + GetWeapon().GetType().ToString());
 
             // Create specialized pool
-            bulletPool = poolGo.AddComponent<Pooling>();
-            bulletPool.CreatePool(bulletPrefab, poolSize);
+            projectilePool = poolGo.AddComponent<Pooling>();
+            projectile = bulletPrefab.GetComponent<AProjectile>();
+
+            projectilePool.CreatePool(projectile, poolSize);
         }
     }
 }
